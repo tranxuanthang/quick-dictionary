377fc57612a7f9e3cd21e0a6ed532af74a304f52
tranxuanthang <tranthang.yb@outlook.com>
2018-02-21 13:57:39 +0700

    Some optimizations

--- quick-dict-main.js
+++ quick-dict-main.js
@@ -24,13 +24,13 @@ function makeRequest(method, url) {
 	});
 }
 
-async function wiktionary(keyword, language){
-	let url = 'https://' + language + '.wiktionary.org/w/api.php?action=parse&format=json&contentmodel=wikitext&redirects=true&prop=text&page=' + keyword;
-	let rawResult = await makeRequest('GET',url);
+async function wiktionary(keyword, language) {
+	let url = "https://" + language + ".wiktionary.org/w/api.php?action=parse&format=json&contentmodel=wikitext&redirects=true&prop=text&page=" + keyword;
+	let rawResult = await makeRequest("GET",url);
 	let jsonResult = JSON.parse(rawResult);
 
 	let error = jsonResult.error;
-	if (error !== undefined) throw new Error('wiktionary_not_found');
+	if (error !== undefined) throw new Error("wiktionary_not_found");
 
 	let htmlResult = jsonResult.parse.text["*"];
 
@@ -38,19 +38,19 @@ async function wiktionary(keyword, language){
 	var replace = "\"//" + language + ".wiktionary.org";
 	var re = new RegExp(replace, "g");
 	htmlResult = htmlResult.replace(re, "\"https://" + language + ".wiktionary.org");
-	htmlResult = htmlResult.replace(/\"\/\/upload.wikimedia.org/g, "\"https://upload.wikimedia.org");
-	htmlResult = htmlResult.replace(/\"\/\/commons.wikimedia.org/g, "\"https://commons.wikimedia.org");
-	htmlResult = htmlResult.replace(/\"\/static\//g, "\"https://" + language + ".wiktionary.org/static/");
-	htmlResult = htmlResult.replace(/\"\/wiki\/(.*?)\"/g, "\"" + location.pathname + "#input=$1\"");
-	htmlResult = htmlResult.replace(/\"\/wiki\//g, "\"https://" + language + ".wiktionary.org/wiki/");
-	htmlResult = htmlResult.replace(/\"\/w\//g, "\"https://" + language + ".wiktionary.org/w/");
+	htmlResult = htmlResult.replace(/"\/\/upload.wikimedia.org/g, "\"https://upload.wikimedia.org");
+	htmlResult = htmlResult.replace(/"\/\/commons.wikimedia.org/g, "\"https://commons.wikimedia.org");
+	htmlResult = htmlResult.replace(/"\/static\//g, "\"https://" + language + ".wiktionary.org/static/");
+	htmlResult = htmlResult.replace(/"\/wiki\/(.*?)"/g, "\"" + location.pathname + "#input=$1\"");
+	htmlResult = htmlResult.replace(/"\/wiki\//g, "\"https://" + language + ".wiktionary.org/wiki/");
+	htmlResult = htmlResult.replace(/"\/w\//g, "\"https://" + language + ".wiktionary.org/w/");
 	return htmlResult;
 }
 
-async function googleTranslate(keyword,language){
+async function googleTranslate(keyword,language) {
 	var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" 
 	+ "auto" + "&tl=" + language + "&dt=t&q=" + encodeURI(keyword);
-	let rawResult = await makeRequest('GET',url);
+	let rawResult = await makeRequest("GET",url);
 	let jsonResult = JSON.parse(rawResult);
 	return jsonResult[0].map(
 		function(element){
@@ -59,37 +59,43 @@ async function googleTranslate(keyword,language){
 	).join("");
 }
 
-async function printResult(keyword,language){
-	keyword = decodeURI(keyword).replace(/_/g, ' ');
-	document.getElementById('inputframe').value = keyword;
-	let wiktionaryResult;
+async function printResult(keyword,language) {
+	// Replace underscores "_" with spaces " "
+	keyword = decodeURI(keyword).replace(/_/g, " ");
+
+	// Put the searching keyword to #inputframe
+	document.getElementById("inputframe").value = keyword;
+
+	// Get wiktionary result and put it to #result
 	try{
-		wiktionaryResult = await wiktionary(keyword,language);
-		document.getElementById('result').innerHTML = wiktionaryResult;
+		let wiktionaryResult = await wiktionary(keyword,language);
+		document.getElementById("result").innerHTML = wiktionaryResult;
 
 	}
+
+	// If detect any error, try to get the meaning with Google Translate
 	catch (error){
-		document.getElementById('result').innerHTML = chrome.i18n.getMessage("popup_wiktionary_not_found")+` Error: ${error}`;
+		document.getElementById("result").innerHTML = chrome.i18n.getMessage("popup_wiktionary_not_found")+` Error: ${error}`;
+		let googleTranslateResult = await googleTranslate(keyword, language);
+		document.getElementById("result").innerHTML = googleTranslateResult;
 	}
 }
 
-document.addEventListener('DOMContentLoaded',function(event){
+/* When page is ready */
+document.addEventListener("DOMContentLoaded",function () {
+	// If the search form is submitted
 	document.getElementById("searchform").addEventListener("submit",async function(event){
 		let inputText = document.getElementById("inputframe").value;
-		printResult(inputText,'vi');
-		event.preventDefault();
-	});
-	document.getElementById("searchform").addEventListener("submit",async function(event){
-		let inputText = document.getElementById("inputframe").value;
-		printResult(inputText,'vi');
+		printResult(inputText,"vi");
 		event.preventDefault();
 	});
 
-	document.getElementById('result').addEventListener('click', function(event){
+	// When the user clicked on any URL in the (wiktionary) result
+	document.getElementById("result").addEventListener("click", function (event) {
 		if(event.target.href === undefined) return;
-		let qdInput = event.target.hash.substr(1).split('=')[1];
+		let qdInput = event.target.hash.substr(1).split("=")[1];
 		if(qdInput === undefined) return;
-		printResult(qdInput,'vi');
+		printResult(qdInput,"vi");
 		event.preventDefault();
 	});
 });
