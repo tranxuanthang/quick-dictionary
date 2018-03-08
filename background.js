15cbb91cb9cecda98f8dbd1e2eda99a2caae236a
Tran Xuan Thang <tranthang.yb@outlook.com>
2018-03-08 07:27:16 +0700

    Official v2.0.0, context menu, hotkey,...

--- background.js
+++ background.js
@@ -1,3 +1,36 @@
 browser.browserAction.onClicked.addListener(function() {
 	browser.sidebarAction.open();
 });
+
+browser.menus.create({
+	id: "context_find_meaning",
+	title: chrome.i18n.getMessage("translate")+" %s",
+	contexts: ["selection"]
+});
+
+browser.menus.onClicked.addListener(function (info, tab) {
+	if (info.menuItemId == "context_find_meaning") {
+		console.log(info.selectionText);
+		browser.tabs.sendMessage(
+			tab.id,
+			{type: "show_meaning", text: info.selectionText}
+		);
+	}
+});
+
+browser.runtime.onInstalled.addListener(function () {
+	browser.tabs.create({
+		url: "https://tranxuanthang.github.io/quick-dictionary/index.html"
+	});
+});
+
+browser.commands.onCommand.addListener(async function (cmd) {
+	let tab = await browser.tabs.query({currentWindow: true, active: true});
+	console.log(tab[0]);
+	if (cmd == "show_meaning") {
+		browser.tabs.sendMessage(
+			tab[0].id,
+			{ type: "show_meaning" }
+		);
+	}
+});
--- content.js
+++ content.js
@@ -24,7 +24,7 @@ function getMouseCoords(event) {
 	// Use event.pageX / event.pageY here
 }
 
-function showQuickButton(x, y) {
+function showQuickButton(x, y, display = true) {
 	let txtSel = getSelection().toString();
 
 	let quickButton = document.getElementById("qdExt_quickButton");
@@ -33,10 +33,45 @@ function showQuickButton(x, y) {
 			quickButton.setAttribute("style", "display: none;");
 		}
 	} else {
-		quickButton.setAttribute("style", `top: ${y - 48}px; left: ${x - 8}px;`);
+		if (display == true) {
+			quickButton.setAttribute("style", `top: ${y - 48}px; left: ${x - 8}px;`);
+		} else {
+			quickButton.setAttribute("style", `top: ${y - 48}px; left: ${x - 8}px; display: none;`);
+		}
+		
 		quickButton.setAttribute("data-selected-text", txtSel);
 		quickButton.setAttribute("data-coord-x", x);
 		quickButton.setAttribute("data-coord-y", y);
+	}
+}
+
+async function smartMeaningShow(selectedText) {
+	// Try sending message to the sidebar
+	let sending;
+	try {
+		sending = await browser.runtime.sendMessage({ type: "search_for_meaning", text: selectedText });
+	}
+	catch (error) {
+		sending = false;
+	}
+
+	// If the sidebar is not available, show "quick popup" instead
+	if (sending != true) {
+		// Show the cover
+		let cover = document.getElementById("qdExt_cover");
+		cover.setAttribute("style", "display: block;");
+
+		// Take mouse coordinate (saved from "quick button" element attribute)
+		let quickButton = document.getElementById("qdExt_quickButton");
+		let x = Number(quickButton.getAttribute("data-coord-x"));
+		let y = Number(quickButton.getAttribute("data-coord-y"));
+
+		// Create the "quick popup"
+		let quickPopup = document.createElement("iframe");
+		quickPopup.setAttribute("id", "qdExt_quickPopup");
+		quickPopup.src = browser.extension.getURL(`/quickpopup.html#input=${selectedText}`);
+		quickPopup.setAttribute("style", `top: ${y + 16}px; left: ${x}px;`);
+		document.getElementsByTagName("body")[0].appendChild(quickPopup);
 	}
 }
 
@@ -58,30 +93,8 @@ function showQuickButton(x, y) {
 		// Get selected text
 		let selectedText = quickButton.getAttribute("data-selected-text").trim();
 
-		// Send message to the sidebar (assuming sidebar is available)
-		let sending;
-		try{
-			sending = await browser.runtime.sendMessage({ type: "search_for_meaning", text: selectedText });
-		}
-		catch(error) {
-			sending = false;
-		}
-		console.log("sending: ", sending);
-		if(sending != true) {
-			// Show the cover
-			cover.setAttribute("style", "display: block;");
-
-			// Take mouse coordinate (saved from "quick button" element attribute)
-			let x = Number(quickButton.getAttribute("data-coord-x"));
-			let y = Number(quickButton.getAttribute("data-coord-y"));
-
-			// Create the "quick popup"
-			let quickPopup = document.createElement("iframe");
-			quickPopup.setAttribute("id", "qdExt_quickPopup");
-			quickPopup.src = browser.extension.getURL(`/quickpopup.html#input=${selectedText}`);
-			quickPopup.setAttribute("style", `top: ${y + 16}px; left: ${x}px;`);
-			document.getElementsByTagName("body")[0].appendChild(quickPopup);
-		}
+		// Show the result
+		smartMeaningShow(selectedText);
 	});
 
 	// Listen click event to "cover" element
@@ -98,17 +111,22 @@ function showQuickButton(x, y) {
 		let savedData = await browser.storage.local.get({
 			quickButton: true
 		});
+		let display = true;
 		if (savedData.quickButton == false) {
-			let quickButton = document.getElementById("qdExt_quickButton");
-			if (quickButton) {
-				quickButton.setAttribute("style", "display: none;");
-			}
-			return;
+			display = false;
 		}
 		
 		let coords = getMouseCoords(event);
 		setTimeout(function () {
-			showQuickButton(coords.x, coords.y);
+			showQuickButton(coords.x, coords.y, display);
 		}, 50);
+	});
+
+	browser.runtime.onMessage.addListener(function(request) {
+		// Get selected text
+		let selectedText = quickButton.getAttribute("data-selected-text").trim();
+
+		// Show the result
+		smartMeaningShow(selectedText);
 	});
 })();
